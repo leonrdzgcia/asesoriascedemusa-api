b871044113fc3a2e6533662aaf334306324326d3
leordzgcia <leongarzagarza@hotmail.com>
2024-07-11 01:07:12 -0600

    properties actualizacion

--- src/main/java/com/example/demo/repositories/UsuarioRepositoryMail.java
+++ src/main/java/com/example/demo/repositories/UsuarioRepositoryMail.java
@@ -1,2 +1,13 @@
-package com.example.demo.repositories;public interface UsuarioRepositoryMail {
+package com.example.demo.repositories;
+
+import com.example.demo.models.UsuarioMailModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepositoryMail extends JpaRepository<UsuarioMailModel, Long> {
+
+    Optional<UsuarioMailModel> findByEmail(String email);
+    Optional<UsuarioMailModel> findByResetToken(String resetToken);
+
 }
--- src/main/java/com/example/demo/services/RestablecerContrasenaService.java
+++ src/main/java/com/example/demo/services/RestablecerContrasenaService.java
@@ -1,6 +1,6 @@
 package com.example.demo.services;
 
-import com.example.demo.models.Usuario;
+import com.example.demo.models.UsuarioMailModel;
 import com.example.demo.repositories.UsuarioRepositoryMail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 import java.util.UUID;
 
+@Service
 public class RestablecerContrasenaService {
 
     @Autowired
@@ -19,32 +20,33 @@ public class RestablecerContrasenaService {
     private JavaMailSender mailSender;
 
     public void enviarCorreoRestablecimiento(String email) {
-        Optional<Usuario> optionalUsuario = usuarioRepository.findByEmail(email);
+        /*Optional<UsuarioMailModel> optionalUsuario = usuarioRepository.findByEmail(email);
         if (!optionalUsuario.isPresent()) {
             throw new RuntimeException("Usuario no encontrado");
         }
 
-        Usuario usuario = optionalUsuario.get();
+        UsuarioMailModel usuario = optionalUsuario.get();
         usuario.setResetToken(UUID.randomUUID().toString());
-        usuarioRepository.save(usuario);
+        usuarioRepository.save(usuario);*/
 
         SimpleMailMessage correo = new SimpleMailMessage();
-        correo.setTo(usuario.getEmail());
+        //correo.setTo(usuario.getEmail());
+        correo.setTo("leongarzagarza@hotmail.com");
         correo.setSubject("Solicitud de restablecimiento de contraseña");
         correo.setText("Para restablecer su contraseña, haga clic en el siguiente enlace: "
-                + "http://localhost:8080/restablecer?token=" + usuario.getResetToken());
+                + "http://localhost:8080/restablecer?token=" );
 
         mailSender.send(correo);
     }
 
-    public Optional<Usuario> obtenerUsuarioPorToken(String token) {
+    public Optional<UsuarioMailModel> obtenerUsuarioPorToken(String token) {
         return usuarioRepository.findByResetToken(token);
     }
 
-    public void actualizarContrasena(Usuario usuario, String nuevaContraseña) {
-        usuario.setResetToken(null);
+    public void actualizarContrasena(UsuarioMailModel usuario, String nuevaContraseña) {
+        /*usuario.setResetToken(null);
         // Aquí deberías cifrar la contraseña antes de guardarla
         usuario.setPassword(nuevaContraseña);
-        usuarioRepository.save(usuario);
+        usuarioRepository.save(usuario);*/
     }
 }
