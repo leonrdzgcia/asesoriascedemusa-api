a043af1b47c4837525e34fcff9dc38783e26c55a
leordzgcia <leongarzagarza@hotmail.com>
2024-08-17 10:45:49 -0600

    REVERS CONFIG EMAIL

--- src/main/java/com/example/demo/models/UsuarioMailModel.java
+++ src/main/java/com/example/demo/models/UsuarioMailModel.java
@@ -7,33 +7,26 @@ import javax.persistence.Id;
 
 @Entity
 public class UsuarioMailModel {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String email;
     private String resetToken;
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getResetToken() {
         return resetToken;
     }
-
     public void setResetToken(String resetToken) {
         this.resetToken = resetToken;
     }
--- src/main/java/com/example/demo/repositories/UsuarioRepositoryMail.java
+++ src/main/java/com/example/demo/repositories/UsuarioRepositoryMail.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UsuarioRepositoryMail extends JpaRepository<UsuarioMailModel, Long> {
-
     Optional<UsuarioMailModel> findByEmail(String email);
     Optional<UsuarioMailModel> findByResetToken(String resetToken);
-
 }
